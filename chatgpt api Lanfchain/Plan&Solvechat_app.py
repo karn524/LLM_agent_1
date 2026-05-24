b269c1aeab6aa93e08a5cfcea5b3ace06aa74913
karn524 <kiryu@katsukawa.jp>
2026-05-24 16:35:32 +0900

    again update

---
+++ chatgpt api Lanfchain/Plan&Solvechat_app.py
@@ -0,0 +1,86 @@
+from pydantic import BaseModel, Field
+from langchain_core.runnables.base import Runnable
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+# Plam-and-Solveに利用するモデル
+class ActionItem(BaseModel):
+    action_name: str = Field(description="アクション名")
+    action_description: str = Field(description="アクションの詳細")
+
+class Plan(BaseModel):
+    """アクションプランを格納する"""
+    problem: str = Field(description="問題の説明")
+    actions: list[ActionItem] = Field(description="実行すべきアクションリスト")
+
+class ActionResult(BaseModel):
+    """実行時の考えと結果を格納する"""
+
+    thoughs: str = Field(description="検討内容")
+    result: str = Field(description="結果")
+
+
+# 個別タスクの実行Runnableを作成
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_openai.output_parsers.tools import PydanticToolsParser
+
+ACTION_PROMPT = """問題をアクションに分解して解いています。
+これまでのアクションの結果と、次に行うべきアクションを示すので、実際にアクションを実行してその結果を報告してください。
+# 問題
+{problem}
+# アクションプラン
+{action_items}
+# これまでのアクションの結果
+{action_results}
+# 次のアクション
+{next_action}"""
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+llm_action = llm.binf_tools([ActionResult], tool_choice="AtionResult")
+action_parser = PydanticToolsParser(tools=[ActionItem], first_tool_only=True)
+
+action_prompt = PromptTemplate.from_template(ACTION_PROMPT)
+action_runnable = action_prompt | llm_action | action_parser
+
+# action_loop関数
+from langchain_core.messages import AIMessage
+
+def action_loop(action_plan: Plan) -> AIMessage:
+    problem = action_plan.problem
+    actions = action_plan.actions
+
+    #1 計画の全体像を箇条書きにする
+    action_items = "\n".join(["* " + action.action_name for action in actions])
+    action_results_str = ""
+
+    #2 ActionItemを取り出すループ
+    for i, action in enumerate(actions):
+        print("="*20)
+        print(f"[{i+1}/{len(actions)}]以下のアクションを実行します。")
+        print(action.action_name)
+
+        #3 次のアクションの説明を作成
+        next_action = f"*{action.action_name} \n{action.action_description}"
+        response = action_runnable.invoke(dict(problem=problem, action_items=action_items, action_results=action_results_str, next_action=next_action))
+
+        #4 実行結果を追記する
+        action_results_str += f"*{action.action_name} \n{response.result}\n"
+
+        print("-" *10 + "検討内容" + "-" *10)
+        print(response.thoughts)
+        print("-" *10 + "結果" + "-" *10)
+        print(response.result)
+
+        #5 実行結果の全体をAIMessageとして返す
+        return AIMessage(action_results_str)
+
+
+# タスクの複雑さによる分岐
+plan_parser= PydanticToolsParser(tools=[Plan], first_tool_only=True)
+
+def route(ai_message: AIMessage) -> Runnable | AIMessage:
+    if ai_message.response_metadata["finish_reason"] == "tool_calls":
+        return plan_parser | action_loop
+    else:
+        return ai_message
+    
